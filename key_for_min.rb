dc974c6045bc704210e75bb68a28771a3e7338f1
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 18:36:21 +0000

    Automatically backed up by Learn

---
+++ key_for_min.rb
@@ -0,0 +1,14 @@
+def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil
+  end
+  lowest = ""
+  new_key = []
+  name_hash.map do |k, v|
+    if lowest == '' || lowest.to_i > v
+      lowest = v.to_s
+      new_key[0] = k 
+    end
+  end
+  new_key[0]
+end
